6c3d40eeddb363cd60f0ff7468c67f5e78cf523e
seonmin5 <seonmin5555@gmail.com>
2024-12-02 19:00:48 +0900

    Feat/getInfo: 검색 아이콘 button 추가

--- components/button/searchBar.jsx
+++ components/button/searchBar.jsx
@@ -14,7 +14,7 @@ const SearchBar = ({placeholder = "여기에 내용을 검색해 보세요", onS
     };
 
     return (
-        <div className="relative mb-4">
+        <div className="relative w-64">
             <input
                 type="text"
                 value={query}
@@ -23,11 +23,11 @@ const SearchBar = ({placeholder = "여기에 내용을 검색해 보세요", onS
                 placeholder={placeholder}
                 className="w-64 px-4 py-2 pr-10 border bg-[#F2F4F6] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3081F7]"
             />
-            <img
-                src="/icons/magnifying-glass.png"
-                alt="검색 아이콘"
-                className="absolute left-64 top-1/2 transform -translate-y-1/2 w-8 h-8 cursor-pointer"
-            />
+            <button className="absolute left-[260px] top-1/2 transform -translate-y-1/2 w-8 h-8">
+                <img
+                    src="/icons/magnifying-glass.png" alt="검색 아이콘" className="w-full h-full cursor-pointer"
+                />
+            </button>
         </div>
     );
 }
