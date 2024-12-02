bf06912a7a4f40d9cbacc8a6ffd807ebba1e5475
seonmin5 <seonmin5555@gmail.com>
2024-12-02 14:16:13 +0900

    Feat/dev: icons png 파일 추가, 폰트 추가, 사용하지 않는 page 삭제

--- app/(nav)/card-applicants/page.jsx
+++
@@ -1,9 +0,0 @@
-const CardApplicants = () => {
-  return (
-    <div>
-      카드 신청 관리 페이지
-    </div>
-  );
-}
-
-export default CardApplicants;
--- app/(nav)/card-details/page.jsx
+++
@@ -1,9 +0,0 @@
-const CardDetails = () => {
-  return (
-    <div>
-      카드 정보 관리 페이지
-    </div>
-  );
-}
-
-export default CardDetails;
