2a03c808a410260914761cb85a469eca930f2c5a
seonmin5 <seonmin5555@gmail.com>
2024-12-02 19:04:14 +0900

    Feat/getInfo: MemberSearch page 추가

--- components/information/memberSearch.jsx
+++ components/information/memberSearch.jsx
@@ -1,6 +1,31 @@
+'use client'
+import SearchBar from "../../components/button/searchBar";
+import FilterButton from "../../components/button/filterButton";
+import FilterConditions from "../../components/button/filterConditions";
+import {useState} from "react";
+
 const MemberSearchPage = () => {
+    const [showFilter, setShowFilter] = useState(false);
+    const handleSearch = (query) => {
+        console.log(`검색어: ${query}`);
+    };
+    const toggleFilter= () => {
+        setShowFilter((prev) => !prev);
+    }
+
     return (
         <div>
+            <div className="p-6">
+                <div className="flex space-x-10 items-center justify-end">
+                    <SearchBar onSearch={handleSearch}/>
+                    <FilterButton onClick={toggleFilter}/>
+                </div>
+                {showFilter && (
+                    <div className="absolute right-6 z-10">  {/* 필터 패널의 위치 조정 */}
+                        <FilterConditions currentPage="members"/>
+                    </div>
+                )}
+            </div>
             <p>이용자 조회 탭입니다.</p>
         </div>
     );
