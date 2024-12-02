835dc36f0a43f93f49356645b4367684d8ee651e
seonmin5 <seonmin5555@gmail.com>
2024-12-02 16:13:35 +0900

    Feat/getInfo: searchBar 추가

---
+++ components/button/searchBar.jsx
@@ -0,0 +1,35 @@
+'use client'
+
+import {useState} from "react";
+
+const SearchBar = ({placeholder = "여기에 내용을 검색해 보세요", onSearch}) => {
+    const [query, setQuery] = useState("");
+    const handleInputChange = (e) => {
+        setQuery(e.target.value);
+    };
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter" && onSearch) {
+            onSearch(query);
+        }
+    };
+
+    return (
+        <div className="relative mb-4">
+            <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                placeholder={placeholder}
+                className="w-64 px-4 py-2 pr-10 border bg-[#F2F4F6] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3081F7]"
+            />
+            <img
+                src="/icons/magnifying-glass.png"
+                alt="검색 아이콘"
+                className="absolute left-64 top-1/2 transform -translate-y-1/2 w-8 h-8 cursor-pointer"
+            />
+        </div>
+    );
+}
+
+export default SearchBar;
