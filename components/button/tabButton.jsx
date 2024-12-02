986cf0d510b1a148ef063908baba8b587f1b0058
seonmin5 <seonmin5555@gmail.com>
2024-12-02 15:34:28 +0900

    Feat/getInfo: button Tabs 추가

---
+++ components/button/tabButton.jsx
@@ -0,0 +1,34 @@
+'use client'
+import {useState} from "react";
+
+const Tabs = ({tabs, initialIndex = 0, onChange, children}) => {
+    const [activeIndex, setActiveIndex] = useState(initialIndex);
+    const handleTabClick = (index) => {
+        setActiveIndex(index);
+        if (onChange) onChange(index);
+    };
+
+    return (
+        <div>
+            <div role={"tablist"} className="flex space-x-4 border-b">
+                {tabs.map((tab, index) => (
+                    <button
+                        key={index}
+                        role="tab"
+                    aria-selected={activeIndex === index}
+                    onClick={() => handleTabClick(index)}
+                    className={`py-2 px-4 text-gray-600 hover:text-[#3081F7] ${
+                        activeIndex === index ? "border-b-4 border-[#3081F7] font-semibold text-[#3081F7]" : ""
+                    }`}>
+                        {tab}
+                    </button>
+                ))}
+            </div>
+            <div role="tabpanel" className="mt-4">
+                {children[activeIndex]}
+            </div>
+        </div>
+    )
+}
+
+export default Tabs;
