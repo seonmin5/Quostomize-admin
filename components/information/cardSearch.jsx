713a510a89f26c3640fc099be158b9fa92ff3193
seonmin5 <seonmin5555@gmail.com>
2024-12-02 19:03:45 +0900

    Feat/getInfo: CardSearch page 추가

--- components/information/cardSearch.jsx
+++ components/information/cardSearch.jsx
@@ -1,12 +1,31 @@
 'use client'
 import SearchBar from "../../components/button/searchBar";
+import FilterButton from "../../components/button/filterButton";
+import FilterConditions from "../../components/button/filterConditions";
+import {useState} from "react";
 
 const CardSearchPage = () => {
+    const [showFilter, setShowFilter] = useState(false);
     const handleSearch = (query) => {
         console.log(`검색어: ${query}`);
-    };    return (
+    };
+    const toggleFilter= () => {
+        setShowFilter((prev) => !prev);
+    }
+
+    return (
         <div>
-            <SearchBar onSearch={handleSearch} />
+            <div className="p-6">
+                <div className="flex space-x-10 items-center justify-end">
+                    <SearchBar onSearch={handleSearch}/>
+                    <FilterButton onClick={toggleFilter}/>
+                </div>
+                {showFilter && (
+                    <div className="absolute right-6 z-10">  {/* 필터 패널의 위치 조정 */}
+                        <FilterConditions currentPage="cards"/>
+                    </div>
+                )}
+            </div>
             <p>카드 조회 탭입니다.</p>
         </div>
     );
