14c03c25a9dfd29e44e0a446f4ef6b1316432500
seonmin5 <seonmin5555@gmail.com>
2024-12-02 14:19:07 +0900

    Refactor/dev: sideNav 사이í 사이트명 및 NavMenuItem 추가

--- components/sideNav/sideNav.jsx
+++ components/sideNav/sideNav.jsx
@@ -1,27 +1,31 @@
-import Link from "next/link";
 import NavMenuItem from "./navMenuItem";
 
 const SideNav = async () => {
   return (
-    <div className="w-full h-full sticky top-0 left-0 bg-gray-800 text-white">
-      <div className="px-4 pt-9 pb-4 h-48 border-white border-2">
-        <div className="mb-4">로고</div>
-        <div>현재 로그인한 사용자 이름</div>
-        <div>현재 로그인한 사용자 아이디</div>
+      <div className="w-60 min-h-screen bg-[#2C2C2C]">
+          <div className="p-8 border-b border-[#000000] text-white">
+              <div className="flex items-center mb-6">
+                  <div className="text-xl font-bold flex items-center">
+                      WON 커스터마이징
+                  </div>
+              </div>
+              <div className="text-gray-300 text-sm">로그인한 사용자 이름</div>
+              <div className="text-gray-300 text-sm">로그인한 사용자 아이디</div>
+          </div>
+          <div className="">
+              <h2 className="pt-8 pr-8 pl-8 pb-6 font-bold text-white text-lg">사이트 관리</h2>
+              <ul className="space-y-1">
+                  <NavMenuItem menuName={"알림 설정"} menuHref={"notifications"} iconSrc={"/icons/notification.png"}/>
+                  <NavMenuItem menuName={"정보 조회"} menuHref={"information"} iconSrc={"/icons/information.png"}/>
+                  <NavMenuItem menuName={"이용자 관리"} menuHref={"members"} iconSrc={"/icons/user.png"}/>
+                  <NavMenuItem menuName={"카드 관리"} menuHref={"cards"} iconSrc={"/icons/card.png"}/>
+                  <NavMenuItem menuName={"가맹점 관리"} menuHref={"franchises"} iconSrc={"/icons/franchise.png"}/>
+                  <NavMenuItem menuName={"대시보드"} menuHref={"https://grafana.com/products/cloud/"}
+                               iconSrc={"/icons/dashboard.png"}/>
+                  <NavMenuItem menuName={"환경설정"} menuHref={"setting"} iconSrc={"/icons/setting.png"}/>
+              </ul>
+          </div>
       </div>
-      <ul className="pt-4 space-y-4">
-        <NavMenuItem menuName={"카드 정보 관리"} menuHref={"card-details"} />
-        <NavMenuItem menuName={"카드 신청 관리"} menuHref={"card-applicants"} />
-        <NavMenuItem menuName={"회원 관리"} menuHref={"members"} />
-        <NavMenuItem menuName={"가맹점 관리"} menuHref={"franchises"} />
-        <NavMenuItem menuName={"알림 보내기"} menuHref={"notifications"} />
-        <li>
-          <Link href="#" className="flex items-center space-x-3 text-xl rounded-md bg-transparent px-3 py-2 hover:bg-gray-600 hover:font-semibold " target="_blank">
-            <span>모니터링</span>
-          </Link>
-        </li>
-      </ul>
-    </div>
   );
 }
 
