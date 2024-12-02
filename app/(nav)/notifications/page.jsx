73fcdcdc8261190c58022c325bb2e32ef0358885
Kee0304 <kee0304@gmail.com>
2024-12-03 00:02:43 +0900

    Feat/MailNotification: css 조정

--- app/(nav)/notifications/page.jsx
+++ app/(nav)/notifications/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import 'react-quill-new/dist/quill.snow.css';
+import './editor.css';
 
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
@@ -11,35 +12,57 @@ const QuillWrapper = dynamic(() => import('react-quill-new'), {
   loading: () => <p>Loading ...</p>,
 })
 
+const modules = {
+  toolbar: [
+    [{ 'header': [1, 2, false] }],
+    ['bold', 'italic', 'underline','strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image'],
+    ['clean']
+  ]
+}
+
 const Notifications = () => {
   const [title, setTitle] = useState("");
   const [htmlContents, setHtmlContents] = useState("");
 
   const updateTitle = (event) => {
     const newTitle = event.target.value;
-    setTitle(newTitle);
+    if (newTitle.length <= 255) {
+      setTitle(newTitle);
+    }
   }
 
   const updateContents = (newContents) => {
     setHtmlContents(newContents);
   }
 
-  
+  const importantStyle = {
+    maxHeight: "500px !important",
+    overflowY: "auto !important"
+  }
 
   return (
-    <div className="w-full">
+    <div className="w-full max-w-full h-full">
       <PageHeader />
-      <div className="p-8">
-        <div className="flex gap-5 mb-10">
-          <div className="text-xl font-semibold">
+      <div className=' mt-6 px-10 flex justify-end'>
+        <div 
+          className='cursor-pointer bg-primary font-semibold text-white w-32 h-10 text-center leading-10 align-middle rounded-lg'
+        >
+          알림 보내기
+        </div>
+      </div>
+      <div className="p-8 h-[calc(100%-5rem)]">
+        <div className="flex gap-5 bg-content-secondary1 h-10 items-center px-2 border-2 border-content-secondary3">
+          <div className="font-semibold">
             제목
           </div>
           <input 
-            className="border-2 rounded-md grow text-xl h-8 leading-8 px-1" 
+            className="border-2 rounded-md grow text-xl h-8 leading-8 px-1 bg-content-secondary2" 
             onChange={(e) => updateTitle(e)}
           />
         </div>
-        <QuillWrapper onChange={updateContents} theme="snow" />
+        <QuillWrapper onChange={updateContents} theme="snow" modules={modules} style={importantStyle} placeholder="내용을 입력해주세요"/>
       </div>
     </div>
   ); 
