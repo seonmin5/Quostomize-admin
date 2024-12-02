9739a5566b45ca5cd85cd12d91c592eb2154f399
seonmin5 <seonmin5555@gmail.com>
2024-12-02 16:12:36 +0900

    Feat/getInfo: 카드 조회 탭에 SearchBar 추가

--- components/information/cardSearch.jsx
+++ components/information/cardSearch.jsx
@@ -1,6 +1,12 @@
+'use client'
+import SearchBar from "../../components/button/searchBar";
+
 const CardSearchPage = () => {
-    return (
+    const handleSearch = (query) => {
+        console.log(`검색어: ${query}`);
+    };    return (
         <div>
+            <SearchBar onSearch={handleSearch} />
             <p>카드 조회 탭입니다.</p>
         </div>
     );
