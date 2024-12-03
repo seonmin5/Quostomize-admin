a149a6781bb3ef7ab2e162eab83f0954cb6765e5
seonmin5 <seonmin5555@gmail.com>
2024-12-03 17:50:51 +0900

    Feat/updatePermission: Member page에 Tab과 MembersEditPage 추가

--- app/(nav)/members/page.jsx
+++ app/(nav)/members/page.jsx
@@ -1,9 +1,17 @@
+'use client'
+
+import Tabs from "../../../components/button/tabButton";
+import MembersEditPage from "../../../components/members/membersEdit"
+
 const Member = () => {
-  return (
-    <div>
-      이용자 관리 페이지
+    const tabs = ["이용자 관리"];
+    return (
+    <div className="pt-6 pl-8">
+        <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
+            <MembersEditPage />
+        </Tabs>
     </div>
-  );
+  )
 }
 
 export default Member;
