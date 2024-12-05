79e984a5c8291a2fe1706e4f4664135a2f98058c
seonmin5 <seonmin5555@gmail.com>
2024-12-05 20:03:20 +0900

    Feat/updatePermission: components creationPendingColumns 추가

---
+++ components/column/creationPendingColumns.jsx
@@ -0,0 +1,45 @@
+export function CreationPendingColumns() {
+    return [
+        {
+            Header: 'Applicant Email',
+            accessor: 'applicantEmail',
+        },
+        {
+            Header: 'Applicant Name',
+            accessor: 'applicantName',
+        },
+        {
+            Header: 'English Name',
+            accessor: 'englishName',
+        },
+        {
+            Header: 'Home Address',
+            accessor: 'homeAddress',
+        },
+        {
+            Header: 'Home Detail Address',
+            accessor: 'homeDetailAddress',
+        },
+        {
+            Header: 'Phone Number',
+            accessor: 'phoneNumber',
+        },
+        {
+            Header: 'Residence Number',
+            accessor: 'residenceNumber',
+        },
+        {
+            Header: 'Shipping Address',
+            accessor: 'shippingAddress',
+        },
+        {
+            Header: 'Shipping Detail Address',
+            accessor: 'shippingDetailAddress',
+        },
+        {
+            Header: 'Zip Code',
+            accessor: 'zipCode',
+        },
+
+    ]
+}
