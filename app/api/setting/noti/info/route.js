84e0797481aaaad4d3a13610f6341a08d0f54ed7
seonmin5 <seonmin5555@gmail.com>
2024-12-06 02:16:01 +0900

    Feat/updatePermission: noti info route.js 추가

---
+++ app/api/setting/noti/info/route.js
@@ -0,0 +1,39 @@
+import {auth} from "../../../../../auth";
+import {NextResponse} from "next/server";
+
+
+export async function GET() {
+
+    const session = await auth();
+    const accessToken = session.accessToken;
+
+    try {
+        const backendResponse = await fetch(`${process.env.SERVER_URL}/v1/api/admin/logs/mail`,
+            {
+                method: 'GET',
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${accessToken}`
+                },
+                credentials: 'include',
+            });
+
+        const result = await backendResponse.json();
+
+        if (!backendResponse.ok) {
+            return NextResponse.json(
+                { message: 'Backend server error' },
+                { status: backendResponse.status }
+            );
+        }
+
+        return NextResponse.json(result);
+
+    } catch (error) {
+        console.error('API Error:', error);
+        return NextResponse.json(
+            {message: '서버 에러'},
+            {status: 500}
+        );
+    }
+}
