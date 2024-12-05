47e940409803cd7528cdf83d84b0070a3b827d82
seonmin5 <seonmin5555@gmail.com>
2024-12-06 04:00:45 +0900

    Design/updatePermission: cafranchisesage ScrollUpButton 추가

--- app/(nav)/franchises/page.jsx
+++ app/(nav)/franchises/page.jsx
@@ -2,14 +2,19 @@
 
 import Tabs from "../../../components/button/tabButton";
 import FranchisesEditPage from "../../../components/franchises/franchisesEdit";
+import ScrollUpButton from "../../../components/button/scrollUpButton";
+
 
 const Franchises = () => {
     const tabs = ["가맹점 관리"];
     return (
-        <div className="pt-6 pl-8">
-            <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
-                <FranchisesEditPage />
-            </Tabs>
+        <div className="pl-2">
+            <div className="pt-6 pl-4 pr-6 pb-2">
+                <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
+                    <FranchisesEditPage/>
+                </Tabs>
+            </div>
+            <ScrollUpButton />
         </div>
     );
 }
