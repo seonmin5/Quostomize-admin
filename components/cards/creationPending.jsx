577a8ec15c51d02612088f9761e5329c7b4a1f43
seonmin5 <seonmin5555@gmail.com>
2024-12-05 20:00:18 +0900

    Refactor/updatePermission: CreationPending GET API 연결

--- components/cards/creationPending.jsx
+++ components/cards/creationPending.jsx
@@ -1,127 +1,88 @@
 'use client'
 import SearchBar from "../../components/button/searchBar";
 import FilterButton from "../../components/button/filterButton";
-import PendingConditions from "../../components/button/pendingConditions";import {useState, useEffect} from "react";
+import PendingConditions from "../../components/button/pendingConditions";
+import {useState, useEffect} from "react";
 import CheckBoxTable from "../../components/table/checkBoxTable"
 import SubmitButtonV2 from "../../components/button/submitButtonV2";
+import {CreationPendingColumns} from "../../components/column/creationPendingColumns"
+import {CardInfo, CardInfoByFilter, CardInfoByKeyword} from "../../service/creationPending/get"
 
 const CreationPendingPage = () => {
-    const [cardData, setCardData] = useState([]);
+    const [cardInfo, setCardInfo] = useState(
+        {
+            content: [],
+            currentPage: 0,
+            totalPage: 1
+    });
+    const [isLoading, setIsLoading] = useState(true);
+    const [filterData, setFilterData] = useState({ page: 0 });
+    const [showFilter, setShowFilter] = useState(false);
+    const [page, setPage] = useState(0);
+    const [selectedRows, setSelectedRows] = useState([]);
     const [error, setError] = useState(null);
-
-    const fetchCardData = async () => {
-        try {
-            const response = await fetch('api/cards/creationPending', {
-                method: "GET",
-                cache: "no-store",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                credentials: "include",
-            });
-
-            if (!response.ok) {
-                const errorResponse = await response.json();
-                throw new Error(errorResponse.message || `HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            setCardData(data.data);
-        } catch (error) {
-            console.error('Error - 카드 불러오기: ', error.message);
-            setError(error.message);
-        }
-    }
+    const param = new URLSearchParams();
 
     useEffect(() => {
-        fetchCardData();
+        const getCardInfo = async () => {
+            setIsLoading(true);
+            try {
+                const response = await CardInfo();
+                setCardInfo(response);
+            } catch (err) {
+                setError("데이터를 불러오는 중 문제가 발생했습니다.");
+                console.error("Error fetching data:", err);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getCardInfo();
     }, []);
 
-    const [showFilter, setShowFilter] = useState(false);
-    const [selectedRows, setSelectedRows] = useState([]);
+    useEffect(() => {
+        if (filterData.page >= 0) {
+            const fetchFilteredData = async () => {
+                try {
+                    const response = await CardInfoByFilter(setCardInfo, param, filterData);
+                    setCardInfo(response);
+                } catch (err) {
+                    console.error("Error fetching filtered data:", err);
+                }
+            };
+            fetchFilteredData();
+        }
+    }, [filterData]);
 
-    const data = Array.isArray(cardData.content)
-        ? cardData.content.map((item, index) => ({
-            number: index + 1,  // 넘버링 (1부터 시작)
-            applicantEmail: item.applicantEmail || '',
-            applicantName: item.applicantName || '',
-            englishName: item.englishName || '',
-            homeAddress: item.homeAddress || '',
-            homeDetailAddress: item.homeDetailAddress || '',
-            phoneNumber: item.phoneNumber || '',
-            residenceNumber: item.residenceNumber || '',
-            shippingAddress: item.shippingAddress || '',
-            shippingDetailAddress: item.shippingDetailAddress || '',
-            zipCode: item.zipCode || '',
-        }))
-        : [];
-
-    const columns = [
-        {
-            Header: 'No',  // 컬럼 제목을 "No"로 변경
-            accessor: 'number',  // 넘버링을 표시할 컬럼으로 'number'를 사용
-        },
-        {
-            Header: 'Applicant Email',
-            accessor: 'applicantEmail',
-        },
-        {
-            Header: 'Applicant Name',
-            accessor: 'applicantName',
-        },
-        {
-            Header: 'English Name',
-            accessor: 'englishName',
-        },
-        {
-            Header: 'Home Address',
-            accessor: 'homeAddress',
-        },
-        {
-            Header: 'Home Detail Address',
-            accessor: 'homeDetailAddress',
-        },
-        {
-            Header: 'Phone Number',
-            accessor: 'phoneNumber',
-        },
-        {
-            Header: 'Residence Number',
-            accessor: 'residenceNumber',
-        },
-        {
-            Header: 'Shipping Address',
-            accessor: 'shippingAddress',
-        },
-        {
-            Header: 'Shipping Detail Address',
-            accessor: 'shippingDetailAddress',
-        },
-        {
-            Header: 'Zip Code',
-            accessor: 'zipCode',
-        },
-    ];
-
-    const handleSearch = (query) => {
-        console.log(`검색어: ${query}`);
+    const handleSearch = async (query) => {
+        const keywordData = {
+            page: page,
+            searchTerm: query
+        }
+        try {
+            const response = await CardInfoByKeyword(setCardInfo, param, keywordData);
+            setCardInfo(response);
+        } catch (err) {
+            console.error("Error searching data:", err);
+        }
     };
 
-    if (error) {
-        return <div>에러가 발생했습니다: {error}</div>;
-    }
-
-    if (!cardData) {
-        return <div>로딩 중</div>;
-    }
-
-    const toggleFilter= () => {
+    const toggleFilter = () => {
         setShowFilter((prev) => !prev);
     }
+
     const handleApprove = () => {
-        const approvedData = selectedRows.map((index) => data[index]);
+        const approvedData = selectedRows.map((index) => cardInfo.content[index]);
         alert(`${approvedData.length}개의 행이 승인되었습니다.`);
         setSelectedRows([]);
     };
+
+    if (isLoading) {
+        return <div>로딩 중...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
 
     return (
         <div>
@@ -133,25 +94,34 @@ const CreationPendingPage = () => {
                         </SubmitButtonV2>
                     </div>
                     <div className="items-center flex space-x-10">
-                        <SearchBar onSearch={handleSearch}/>
+                        <SearchBar onSearch={handleSearch} placeholder="Member Id를 입력해 주세요" onClick={handleSearch}/>
                         <FilterButton onClick={toggleFilter}/>
                     </div>
                 </div>
                 {showFilter && (
                     <div className="absolute right-6 z-10">
-                        <PendingConditions />
+                        <PendingConditions
+                            setFilterData={setFilterData}
+                            page={page}
+                            setPage={setPage}
+                            dataPage={cardInfo.totalPage}
+                        />
                     </div>
                 )}
             </div>
             <CheckBoxTable
-                columns={columns}
-                data={data}
+                columns={CreationPendingColumns()}
+                data={cardInfo.content}
+                setFilterData={setFilterData}
+                filterData={filterData}
+                page={cardInfo.currentPage}
+                setPage={setPage}
+                dataPage={cardInfo.totalPage}
                 selectedRows={selectedRows}
                 setSelectedRows={setSelectedRows}
             />
         </div>
     );
-
-
 }
+
 export default CreationPendingPage;
