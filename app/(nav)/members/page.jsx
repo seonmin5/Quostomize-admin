7c237ba70e228f9ab04806c906a7c9a8c864cdeb
seonmin5 <seonmin5555@gmail.com>
2024-12-06 04:02:01 +0900

    Design/updatePermission: members page ScrollUpButton 추가

--- app/(nav)/members/page.jsx
+++ app/(nav)/members/page.jsx
@@ -2,16 +2,21 @@
 
 import Tabs from "../../../components/button/tabButton";
 import MembersEditPage from "../../../components/members/membersEdit"
+import ScrollUpButton from "../../../components/button/scrollUpButton";
+
 
 const Member = () => {
     const tabs = ["이용자 관리"];
     return (
-    <div className="pt-6 pl-8">
-        <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
-            <MembersEditPage />
-        </Tabs>
-    </div>
-  )
+        <div className="pl-2">
+            <div className="pt-6 pl-4 pr-6 pb-2">
+                <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
+                    <MembersEditPage/>
+                </Tabs>
+            </div>
+            <ScrollUpButton />
+        </div>
+    )
 }
 
 export default Member;
