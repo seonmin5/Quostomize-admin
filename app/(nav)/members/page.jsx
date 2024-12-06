0e1a561f60eb4147a1661ab160af8e72a841b235
seonmin5 <seonmin5555@gmail.com>
2024-12-06 11:27:47 +0900

    Design/AdminFE: members page padding 조정

--- app/(nav)/members/page.jsx
+++ app/(nav)/members/page.jsx
@@ -9,12 +9,12 @@ const Member = () => {
     const tabs = ["이용자 관리"];
     return (
         <div className="pl-2">
-            <div className="pt-6 pl-4 pr-6 pb-2">
+            <div className="pt-8 pl-4 pr-6 pb-2">
                 <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
                     <MembersEditPage/>
                 </Tabs>
             </div>
-            <ScrollUpButton />
+            <ScrollUpButton/>
         </div>
     )
 }
