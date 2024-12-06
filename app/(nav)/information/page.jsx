49fefe6e56938f473dd073ad277dee532d2dc41c
seonmin5 <seonmin5555@gmail.com>
2024-12-06 11:27:22 +0900

    Design/AdminFE: information page padding 조정

--- app/(nav)/information/page.jsx
+++ app/(nav)/information/page.jsx
@@ -12,15 +12,15 @@ const Information = () => {
     const tabs = ["카드 조회", "이용자 조회", "이용 기록 조회", "로그인 로그 조회"];
     return (
         <div className="pl-2">
-            <div className="pt-6 pl-4 pr-6 pb-2">
+            <div className="pt-8 pl-4 pr-6 pb-2">
                 <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
-                    <CardSearchPage />
-                    <MemberSearchPage />
-                    <PaymentSearchPage />
-                    <LoginSearchPage />
+                    <CardSearchPage/>
+                    <MemberSearchPage/>
+                    <PaymentSearchPage/>
+                    <LoginSearchPage/>
                 </Tabs>
             </div>
-            <ScrollUpButton />
+            <ScrollUpButton/>
         </div>
     )
 }
