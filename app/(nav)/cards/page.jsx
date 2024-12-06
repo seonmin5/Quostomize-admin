b7e96e1c7cd8a2c26cb1fe9ea3eff23737955c31
seonmin5 <seonmin5555@gmail.com>
2024-12-06 11:26:42 +0900

    Design/AdminFE: cards apge padding 조정

--- app/(nav)/cards/page.jsx
+++ app/(nav)/cards/page.jsx
@@ -9,7 +9,7 @@ const Card = () => {
     const tabs = ["카드 신청 내역", "정지 신청 내역"];
     return (
         <div className="pl-2">
-            <div className="pt-6 pl-4 pr-6 pb-2">
+            <div className="pt-8 pl-4 pr-6 pb-2">
                 <Tabs tabs={tabs} initialIndex={0} onChange={(index) => console.log(`Tab changed: ${index}`)}>
                     <CreationPendingPage />
                     <CancellationPendingPage />
