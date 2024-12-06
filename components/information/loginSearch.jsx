42f53bd50ba5e0bd11713a1fb5dbec351745a87f
seonmin5 <seonmin5555@gmail.com>
2024-12-06 11:24:38 +0900

    Design/AdminFE: loginSearch Loading 및 SkeletonLoader 적용

--- components/information/loginSearch.jsx
+++ components/information/loginSearch.jsx
@@ -5,33 +5,46 @@ import FilterConditions from "../../components/button/filterConditionByLoginLog"
 import { loginSearchColumn } from "../../components/column/loginSearchColumn"
 import { loginInfo, loginInfoByFilter } from "../../service/apiMethodList/loginSearch/get"
 import { useEffect, useState } from "react";
-
 import DataTable from "../table/loginDataTable";
-import LoadingModal from "../modal/loadingModal";
+import SkeletonLoader from "../../components/spinner/skeletonLoader";
 
 const LoginSearchPage = () => {
     const [loginInfos, setLoginInfos] = useState([])
     const [filterDatas, setFilterData] = useState({})
     const [showFilter, setShowFilter] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [page, setPage] = useState(0);
     const param = new URLSearchParams()
 
     useEffect(() => {
-        try {
-            loginInfo(setLoginInfos)
-        } catch (error) {
-            console.error(error)
-        } finally {
-            setIsLoading(false)
-        }
-    }, [])
+        const fetchLogInfo = async () => {
+            setIsLoading(true);
+            try {
+                await loginInfo(setLoginInfos);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchLogInfo();
+    }, []);
 
     useEffect(() => {
-        filterDatas.page >= 0 ? loginInfoByFilter(setLoginInfos, param, filterDatas) : null
-    }, [filterDatas])
-
-    const columns = loginSearchColumn()
+        if (filterDatas.page >= 0) {
+            const fetchFilteredData = async () => {
+                setIsLoading(true);
+                try {
+                    await loginInfoByFilter(setLoginInfos, param, filterDatas);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setIsLoading(false);
+                }
+            };
+            fetchFilteredData();
+        }
+    }, [filterDatas]);
 
     const toggleFilter = () => {
         setShowFilter((prev) => !prev);
@@ -44,13 +57,18 @@ const LoginSearchPage = () => {
                     <FilterButton onClick={toggleFilter} />
                 </div>
                 {showFilter && (
-                    <div className="absolute right-6 z-10">  {/* 필터 패널의 위치 조정 */}
+                    <div className="absolute right-6 z-10">
                         <FilterConditions currentPage="loginlog" setFilterData={setFilterData} page={page} setPage={setPage} dataPage={loginInfos.totalPages} />
                     </div>
                 )}
             </div>
-            {isLoading && <LoadingModal message={"로딩 중입니다"} isOpen={isLoading} />}
-            {<DataTable columns={columns} data={loginInfos.content} dataPage={loginInfos.totalPages} setFilterData={setFilterData} filterDatas={filterDatas} page={page} setPage={setPage} />}
+            {isLoading ? (
+                <div className="p-6">
+                    <SkeletonLoader />
+                </div>
+            ) : (
+            <DataTable columns={loginSearchColumn()} data={loginInfos.content} dataPage={loginInfos.totalPages} setFilterData={setFilterData} filterDatas={filterDatas} page={page} setPage={setPage} />
+                )}
         </div>
     );
 };
