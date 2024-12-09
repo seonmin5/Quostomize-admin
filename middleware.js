5389d58a6a8b170b503289087a13b41e1f75fa57
Kee0304 <kee0304@gmail.com>
2024-12-09 14:36:42 +0900

    Refactor/Auth: middleware 수정

--- middleware.js
+++ middleware.js
@@ -3,32 +3,12 @@ import { auth } from "./auth";
 
 export default auth((req) => {
     const session = req.auth;
-    const goalURL = req.nextUrl.pathname.slice(1);
 
     if (!session) {  
         console.error('No session, redirecting to login');
-        if (goalURL !== "login") {
-            return NextResponse.redirect(
-                new URL(`/login?to=${goalURL}`, req.url)
-            );  
-        } else {
-            return NextResponse.redirect(
-                new URL(`/login`, req.url)
-            );  
-        }
-    }
-
-    if (!session.memberRole !== "ROLE_ADMIN") {  
-        console.error('Not Admin, redirecting to login');
-        if (goalURL !== "login") {
-            return NextResponse.redirect(
-                new URL(`/login?to=${goalURL}`, req.url)
-            );  
-        } else {
-            return NextResponse.redirect(
-                new URL(`/login`, req.url)
-            );  
-        }
+        return NextResponse.redirect(
+            new URL(`/login`, req.url)
+        );  
     }
     
     return NextResponse.next();
